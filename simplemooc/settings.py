042bec31ee6a35942c2c4ef50f73fb56f73d40d1
Carolini28 <carolinirodrigues28@gmail.com>
2021-01-23 00:46:28 -0400

    add heroku

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,3 +145,7 @@ LOGIN_URL = 'accounts:login'
 LOGIN_REDIRECT_URL = 'core:home'
 LOGOUT_URL = 'accounts:logout'
 AUTH_USER_MODEL = 'accounts.User'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
